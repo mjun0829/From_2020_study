527979899b86637092e4695889acc37adbba047a
jaeseung-lee <kevin0969@naver.com>
2021-01-01 00:13:29 +0900

    클래스 내부 주석 및 구현 중

--- Othello/ljs_NewOthello.cpp
+++ Othello/ljs_NewOthello.cpp
@@ -3,3 +3,34 @@
 #include <vector>
 
 using namespace std;
+
+Block::Block(int NewX, int NewY){
+    Status=EMPTY;
+    IsAccessible=FALSE;
+    x=NewX;
+    y=NewY;
+}
+
+Block::Block(int NewStatus,int NewX,int NewY){
+    Status=NewStatus;
+    IsAccessible=FALSE;
+    x=NewX;
+    y=NewY;
+}
+
+vector<vector<Block>> BoardManager::InitBoard(int NewSize){
+    vector<vector<Block>> NewBoard;
+    for(int i=0;i<Size;i++){
+        for(int j=0;j<Size;j++){
+            if(i==3&&j==3)
+                NewBoard.push_back(Block(WHITE,3,3));
+            if(i==4&&j==4)
+                NewBoard.push_back(Block(WHITE,4,4));
+            if(i==3&&j==4)
+                NewBoard.push_back(Block(WHITE,3,4));
+            if(i==4&&j==3)
+                NewBoard.push_back(Block(WHITE,4,3));
+        }
+    }
+    return NewBoard;
+}
--- Othello/ljs_NewOthello.hpp
+++ Othello/ljs_NewOthello.hpp
@@ -4,78 +4,147 @@
 #include <vector>
 using namespace std;
 
-enum Color { WHITE, BLACK, EMPTY };
-
-class Block {
-  private:
-    int Status;
-    bool IsAccessible;
-    int x;
-    int y;
-
-  public:
-    Block(int NewX, int NewY);
-    Block(int Status, int NewX, int NewY);
-    void SetStatus(int NewStatus);
-    void Access();
-    void Deny();
-
-    bool GetEmpty() const;
-    bool GetStatus() const;
-    bool GetAccess() const;
-    int GetX() const;
-    int GetY() const;
+//한 칸에 상태를 나타내는 열거형
+//Block 클래스의 status 에 들어감.
+enum Color
+{
+  WHITE,
+  BLACK,
+  EMPTY
 };
 
-class BoardManager {
-    private:
-      vector<vector<Block>> Board;
-      static int Blocks;
-      static int Selected;
-      static int AccessibleBlocks;
-      const static int SIZE;
+//한 칸에 대해서 비어있는지, 하얀 돌인지, 검은 돌인지 정보를 포함하고 있는
+//클래스.
+class Block
+{
+private:
+  //현재 돌의 상태를 나타내는 변수
+  //여기에는 enum Color의 멤버만이 올 수 있다.
+  int Status;
+  //Status=EMPTY 일 때, 이 자리에 놓을 수 있는지
+  bool IsAccessible;
+  //칸의 위치
+  int x;
+  int y;
 
-    public:
-      vector<vector<Block>> InitBoard;
-     void SetBlocks(int);
-        void SetSelected(int);
-    void BlockDisplay(vector<vector<Block>> Board) const;
-            void WhichIsAccessible(vector<vector<Block>> Board);
-            bool CheckBlack(int Now, int Jump, vector<vector<Block>> Board);
-            bool CheckWhite(int Now, int Jump, vector<vector<Block>> Board);
-            bool IsNextWall(int Now, int Jump, vector<vector<Block>> Board);
+public:
+  //처음 판을 그릴 때 중앙 4칸을 제외하고 나머지 칸을 만들 때 사용.
+  //Status=EMPTY
+  Block(int NewX, int NewY);
+  //중앙 4칸을 만들 때 사용
+  Block(int NewStatus, int NewX, int NewY);
+  void SetStatus(int NewStatus){Status=NewStatus;}
 
-            void ReverseBlocks();
+  //칸의 IsAccessible을 바꾸는 함수
+  void Access(){IsAccessible=TRUE;}
+  void Deny(){IsAccessible=FALSE;}
+
+  //Accessors
+  bool GetStatus() const {return Status;}
+  bool GetAccess() const {return IsAccessible;}
+  int GetX() const {return X;}
+  int GetY() const {return Y;}
 };
 
-class User {
-    private:
-      int Blocks;
-      bool IsVictory;
-      bool Turn;
-      int Color;
+class BoardManager
+{
+private:
+  //64개의 칸을 저장하는 벡터
+  static vector<vector<Block>> Board;
 
-    public:
-      User(int NewBlocks,bool NewTurn,int NewColor);
-      int GetBlocks() const;
-      bool GetVictory() const;
-      bool GetTurn() const;
-      int GetColor() const;
+  //현재 판에 놓여져있는 돌의 수
+  static int Blocks;
 
-      void SetBlocks(int NewBlocks);
-      void SetVictory(bool NewVictory);
-      void ChangeTurn() { Turn = !Turn; }
+  //유저가 선택한 돌의 위치
+  static int SelectedX;
+  static int SelectedY;
+
+  //유저가 놓을 수 있는 돌의 개수
+  //돌의 개수가 0개면 턴을 바로 넘김
+  static int AccessibleBlocks;
+
+  //판의 가로/세로 줄 크기
+  static int Size;
+
+public:
+      //게임 시작 전 판을 그리는 함수
+  vector<vector<Block>> InitBoard(int NewSize);
+  void SetBlocks(int NewBlocks){Blocks=NewBlocks;}
+  void SetSelected(int NewX,int NewY){SelectedX=NewX; SelectedY=NewY;}
+
+  //Accessors
+  vector<vector<Block>> GetBoard();
+  int GetX() const;
+  int GetY() const;
+  int GetSize() const;
+  int GetAccessibleBlocks() const;
+
+  //판이 어떤 상황인지 보여주는 함수
+  void BlockDisplay(vector<vector<Block>> Board) const;
+
+  //어떤 블록에 돌을 놓을 수 있는지 각 블록의 IsAccessible을 바꾸는 함수
+  void WhichIsAccessible(vector<vector<Block>> Board);
+
+  //Now에서 JumpX, JumpY 방향으로 검정돌 혹은 흰돌이 있는지 판별하는 함수
+  //8방향중에서 하나도 없다면 그 블록의 IsAccessible=false
+  bool CheckBlack(int Now, int JumpX, int JumpY);
+  bool CheckWhite(int Now, int JumpX, int JumpY);
+
+  //Now에서 JumpX, JumpY 만큼 거리에 있는 돌이 접근할 수 있는지 판별하는 함수
+  bool IsNextWall(int Now, int JumpX, int JumpY, vector<vector<Block>> Board);
+
+  //(X,Y)에 돌을 두고 해당하는 돌을 뒤집어주는 함수
+  void ReverseBlocks(int X, int Y, vector<vector<Block>> Board);
 };
 
-class UserManager {
-    private:
-      static User BlackUser;
-      static User WhiteUser;
+class User
+{
+private:
+  //판 위에 유저의 색에 해당하는 돌의 개수
+  int Blocks;
 
-    public:
-      int HowManyWhiteBlocks();
-      void InsertOneBlock();
-      void CheckVictory();
+  //승리 여부
+  bool IsVictory;
+
+  //현재 둘 차례인지
+  bool Turn;
+
+  //유저의 색
+  int Color;
+
+public:
+  //흰 돌의 Turn=true, 검은 돌의 Turn=false
+  User(int NewBlocks, bool NewTurn, int NewColor);
+  int GetBlocks() const;
+  bool GetVictory() const;
+  bool GetTurn() const;
+  int GetColor() const;
+
+  void SetBlocks(int NewBlocks);
+  void SetVictory(bool NewVictory);
+
+  //턴을 바꾸는 함수
+  void ChangeTurn() { Turn = !Turn; }
+};
+
+class UserManager
+{
+private:
+  //유저의 color=BLACK
+  static User BlackUser;
+
+  //유저의 color=WHITE
+  static User WhiteUser;
+
+public:
+  //Color Block이 얼마나 많이 있는지 반환하는 함수
+  int HowManyWhiteBlocks(int Color);
+
+  //유저에게서 블록 위치 하나를 입력받는 함수
+  void InsertOneBlock();
+
+  //Blocks=64 일 때, User의 Blocks가 더 많은 사람이 승리
+  void CheckVictory();
 };
 
 #endif
