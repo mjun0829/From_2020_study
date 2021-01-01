7d4b03af5197aa4ad5c234c9a83fae90b020a88d
jaeseung-lee <kevin0969@naver.com>
2021-01-01 20:29:12 +0900

    옮기기

--- Othello/ljs_NewOthello.cpp
+++ Othello/ljs_NewOthello.cpp
@@ -4,33 +4,33 @@
 
 using namespace std;
 
-Block::Block(int NewX, int NewY){
-    Status=EMPTY;
-    IsAccessible=FALSE;
-    x=NewX;
-    y=NewY;
+Block::Block(int NewX, int NewY) {
+  Status = EMPTY;
+  IsAccessible = false;
+  x = NewX;
+  y = NewY;
 }
 
-Block::Block(int NewStatus,int NewX,int NewY){
-    Status=NewStatus;
-    IsAccessible=FALSE;
-    x=NewX;
-    y=NewY;
+Block::Block(int NewStatus, int NewX, int NewY) {
+  Status = NewStatus;
+  IsAccessible = false;
+  x = NewX;
+  y = NewY;
 }
 
-vector<vector<Block>> BoardManager::InitBoard(int NewSize){
-    vector<vector<Block>> NewBoard;
-    for(int i=0;i<Size;i++){
-        for(int j=0;j<Size;j++){
-            if(i==3&&j==3)
-                NewBoard.push_back(Block(WHITE,3,3));
-            if(i==4&&j==4)
-                NewBoard.push_back(Block(WHITE,4,4));
-            if(i==3&&j==4)
-                NewBoard.push_back(Block(WHITE,3,4));
-            if(i==4&&j==3)
-                NewBoard.push_back(Block(WHITE,4,3));
-        }
+vector<vector<Block>> BoardManager::InitBoard(int NewSize) {
+  vector<vector<Block>> NewBoard;
+  for (int i = 0; i < Size; i++) {
+    for (int j = 0; j < Size; j++) {
+      if (i == 3 && j == 3)
+        NewBoard[i].push_back(Block(WHITE, 3, 3));
+      if (i == 4 && j == 4)
+        NewBoard[i].push_back(Block(WHITE, 4, 4));
+      if (i == 3 && j == 4)
+        NewBoard[i].push_back(Block(WHITE, 3, 4));
+      if (i == 4 && j == 3)
+        NewBoard[i].push_back(Block(WHITE, 4, 3));
     }
-    return NewBoard;
+  }
+  return NewBoard;
 }
--- Othello/ljs_NewOthello.hpp
+++ Othello/ljs_NewOthello.hpp
@@ -1,27 +1,18 @@
 #ifndef __LJS_NEWOTHELLO_HPP__
 #define __LJS_NEWOTHELLO_HPP__
 
-#include <vector>
-using namespace std;
-
 //한 칸에 상태를 나타내는 열거형
-//Block 클래스의 status 에 들어감.
-enum Color
-{
-  WHITE,
-  BLACK,
-  EMPTY
-};
+// Block 클래스의 status 에 들어감.
+enum Color { WHITE, BLACK, EMPTY };
 
 //한 칸에 대해서 비어있는지, 하얀 돌인지, 검은 돌인지 정보를 포함하고 있는
 //클래스.
-class Block
-{
+class Block {
 private:
   //현재 돌의 상태를 나타내는 변수
   //여기에는 enum Color의 멤버만이 올 수 있다.
   int Status;
-  //Status=EMPTY 일 때, 이 자리에 놓을 수 있는지
+  // Status=EMPTY 일 때, 이 자리에 놓을 수 있는지
   bool IsAccessible;
   //칸의 위치
   int x;
@@ -29,27 +20,26 @@ private:
 
 public:
   //처음 판을 그릴 때 중앙 4칸을 제외하고 나머지 칸을 만들 때 사용.
-  //Status=EMPTY
+  // Status=EMPTY
   Block(int NewX, int NewY);
   //중앙 4칸을 만들 때 사용
   Block(int NewStatus, int NewX, int NewY);
-  void SetStatus(int NewStatus){Status=NewStatus;}
+  void SetStatus(int NewStatus) { Status = NewStatus; }
 
   //칸의 IsAccessible을 바꾸는 함수
-  void Access(){IsAccessible=TRUE;}
-  void Deny(){IsAccessible=FALSE;}
+  void Access() { IsAccessible = true; }
+  void Deny() { IsAccessible = false; }
 
-  //Accessors
-  bool GetStatus() const {return Status;}
-  bool GetAccess() const {return IsAccessible;}
-  int GetX() const {return X;}
-  int GetY() const {return Y;}
+  // Accessors
+  bool GetStatus() const { return Status; }
+  bool GetAccess() const { return IsAccessible; }
+  int GetX() const { return x; }
+  int GetY() const { return y; }
 };
 
-class BoardManager
-{
+class BoardManager {
 private:
-  //64개의 칸을 저장하는 벡터
+  // 64개의 칸을 저장하는 벡터
   static vector<vector<Block>> Board;
 
   //현재 판에 놓여져있는 돌의 수
@@ -67,12 +57,15 @@ private:
   static int Size;
 
 public:
-      //게임 시작 전 판을 그리는 함수
+  //게임 시작 전 판을 그리는 함수
   vector<vector<Block>> InitBoard(int NewSize);
-  void SetBlocks(int NewBlocks){Blocks=NewBlocks;}
-  void SetSelected(int NewX,int NewY){SelectedX=NewX; SelectedY=NewY;}
+  void SetBlocks(int NewBlocks) { Blocks = NewBlocks; }
+  void SetSelected(int NewX, int NewY) {
+    SelectedX = NewX;
+    SelectedY = NewY;
+  }
 
-  //Accessors
+  // Accessors
   vector<vector<Block>> GetBoard();
   int GetX() const;
   int GetY() const;
@@ -85,20 +78,19 @@ public:
   //어떤 블록에 돌을 놓을 수 있는지 각 블록의 IsAccessible을 바꾸는 함수
   void WhichIsAccessible(vector<vector<Block>> Board);
 
-  //Now에서 JumpX, JumpY 방향으로 검정돌 혹은 흰돌이 있는지 판별하는 함수
-  //8방향중에서 하나도 없다면 그 블록의 IsAccessible=false
+  // Now에서 JumpX, JumpY 방향으로 검정돌 혹은 흰돌이 있는지 판별하는 함수
+  // 8방향중에서 하나도 없다면 그 블록의 IsAccessible=false
   bool CheckBlack(int Now, int JumpX, int JumpY);
   bool CheckWhite(int Now, int JumpX, int JumpY);
 
-  //Now에서 JumpX, JumpY 만큼 거리에 있는 돌이 접근할 수 있는지 판별하는 함수
+  // Now에서 JumpX, JumpY 만큼 거리에 있는 돌이 접근할 수 있는지 판별하는 함수
   bool IsNextWall(int Now, int JumpX, int JumpY, vector<vector<Block>> Board);
 
   //(X,Y)에 돌을 두고 해당하는 돌을 뒤집어주는 함수
   void ReverseBlocks(int X, int Y, vector<vector<Block>> Board);
 };
 
-class User
-{
+class User {
 private:
   //판 위에 유저의 색에 해당하는 돌의 개수
   int Blocks;
@@ -127,8 +119,7 @@ public:
   void ChangeTurn() { Turn = !Turn; }
 };
 
-class UserManager
-{
+class UserManager {
 private:
   //유저의 color=BLACK
   static User BlackUser;
@@ -137,13 +128,13 @@ private:
   static User WhiteUser;
 
 public:
-  //Color Block이 얼마나 많이 있는지 반환하는 함수
+  // Color Block이 얼마나 많이 있는지 반환하는 함수
   int HowManyWhiteBlocks(int Color);
 
   //유저에게서 블록 위치 하나를 입력받는 함수
   void InsertOneBlock();
 
-  //Blocks=64 일 때, User의 Blocks가 더 많은 사람이 승리
+  // Blocks=64 일 때, User의 Blocks가 더 많은 사람이 승리
   void CheckVictory();
 };
 
